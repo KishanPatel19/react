fdd41a246b0fe04831f4f9bac5397011507e056f
Kishan Patel <kvpatel6351665793@gmail.com>
2026-06-24 22:55:20 +0530

    mini project on context api

---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useContext } from 'react'
+import UserContext from '../context/userContext'
+
+function Profile() {
+ 
+    const {user} = useContext(UserContext)
+
+    if(!user) return <h3>Please Login </h3>
+    return <h3> Welcome {user.username}  </h3>
+}
+
+export default Profile
